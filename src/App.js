16d0f6278299a0b8982bba56aab85d0dbcad58c5
Rafiere <rafapgbr@gmail.com>
2023-02-21 20:58:17 -0300

    feat: add App.js with React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
 const Pet = (props) => {
-  return React.createElement(
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed)
-  );
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
 const App = () => {
@@ -25,6 +28,6 @@ const App = () => {
 
 const container = document.getElementById("root");
 
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 
 root.render(React.createElement(App));
