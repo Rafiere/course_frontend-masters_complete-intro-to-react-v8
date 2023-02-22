bf07a9114cd217037c1eefadc88caf7e86c5ac3d
Rafiere <rafapgbr@gmail.com>
2023-02-22 05:19:38 -0300

    feat: add useState hook.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
+const ANIMALS = ["Bird", "Cat"];
+const BREEDS = [];
+
 const SearchParams = () => {
   /* O primeiro parâmetro da tupla do "useState" é uma variável que armazena
   o estado atual, enquanto que o segundo parâmetro é uma função que serve para atualizarmos o estado do componente. */
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
 
   return (
     <div className="search-params">
@@ -13,6 +20,35 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            {ANIMALS.map((animal) => {
+              return <option key={animal}>{animal}</option>;
+            })}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Animal
+          <select
+            id="breed"
+            disabled={BREEDS.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            {BREEDS.map((breed) => {
+              return <option key={breed}>{breed}</option>;
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
