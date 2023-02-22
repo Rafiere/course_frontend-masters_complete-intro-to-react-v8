6f3f6a8c65282e8a85ba2f7cb3197b2d217f8f0d
Rafiere <rafapgbr@gmail.com>
2023-02-22 05:41:36 -0300

    feat: add useEffect hook to fetch an external API.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,18 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
-const ANIMALS = ["Bird", "Cat"];
+const ANIMALS = ["bird", "cat"];
 const BREEDS = [];
 
 const SearchParams = () => {
   /* O primeiro parâmetro da tupla do "useState" é uma variável que armazena
   o estado atual, enquanto que o segundo parâmetro é uma função que serve para atualizarmos o estado do componente. */
+
+  /* Basicamente, o hook "useState" permite que lidemos com estados da aplicação. */
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
 
+  /* Um "effect" é, basicamente, algo que acontecerá fora do componente. Ele serve para executarmos tarefas fora do ciclo de vida do componente. A maioria das vezes que esse hook é utilizado, é para realizar requisições de APIs. Ele também pode ser utilizado para acessar o local storage, salvar algo na API, salvar algo no local storage e etc. */
+
+  /* Se não passarmos nenhum array como o segundo parâmetro do "useEffect()", a função dele continuará sendo executada sempre que o componente renderizar. Se passarmos um array de dependências vazio como o segundo parâmetro, o "useEffect()" será executado apenas na primeira vez em que o array renderizar. */
+
+  /* Ao inserirmos o estado "animal" no array de dependências, sempre que o valor do estado de "animal" for alterado, a função que está dentro do "useEffect()" será executada, que, nesse caso, é a função "requestPets". */
+
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           <input
             id="location"
@@ -30,6 +60,7 @@ const SearchParams = () => {
               setAnimal(e.target.value);
             }}
           >
+            <option />
             {ANIMALS.map((animal) => {
               return <option key={animal}>{animal}</option>;
             })}
@@ -45,6 +76,7 @@ const SearchParams = () => {
               setBreed(e.target.value);
             }}
           >
+            <option />
             {BREEDS.map((breed) => {
               return <option key={breed}>{breed}</option>;
             })}
@@ -52,6 +84,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          key={pet.id}
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+        />
+      ))}
     </div>
   );
 };
